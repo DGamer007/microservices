e24caa87e357df943592959262a704763eef38a3
DGamer007 <prajapatidhruv266@gmail.com>
2023-07-11 09:51:46 +0530

    Added Auth Module

--- apps/auth/src/auth.controller.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-
-const authController = Router();
-
-authController.get('/example', (req, res) => {
-    res.send({ message: 'Auth Service Success' });
-});
-
-module.exports = authController;
